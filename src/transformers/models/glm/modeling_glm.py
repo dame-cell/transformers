6d0782fd5f563fe32c5251cc59caef9323df9518
DAME RAJEE <doss72180@gmail.com>
2024-11-21 15:07:53 +0000

    finally fix the imports in glm

--- src/transformers/models/glm/modeling_glm.py
+++ src/transformers/models/glm/modeling_glm.py
@@ -42,11 +42,16 @@ from ...utils import (
     add_code_sample_docstrings,
     add_start_docstrings,
     add_start_docstrings_to_model_forward,
+    is_flash_attn_2_available,
     is_flash_attn_greater_or_equal_2_10,
     logging,
     replace_return_docstrings,
 )
 from .configuration_glm import GlmConfig
+
+
+if is_flash_attn_2_available():
+    from ...modeling_flash_attention_utils import _flash_attention_forward
 
 
 logger = logging.get_logger(__name__)
